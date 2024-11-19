9998a3890f9ed2a5abd654b03e796c1c37bbefa2
Kavya-76 <kavyabansal2000@gmail.com>
2024-11-19 23:54:45 +0530

    Home Page Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div className="text-4xl text-center mt-8~">Spend Smart <br />
-    <Button>
-      Login
-    </Button>
+    <div>
+      <Header/>
+      <Hero/>
     </div>
   );
 }
