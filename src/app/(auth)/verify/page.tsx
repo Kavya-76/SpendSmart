8b1a5702f252149a834bf5c19d5ce3eca382831f
Kavya-76 <kavyabansal2000@gmail.com>
2024-11-23 00:03:57 +0530

    Create Basic Structure

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Verify() {
   return (
     <div>
-      Verify
+      Verify Page
     </div>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { log } from "console";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
